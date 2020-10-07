bce247f27ab91ccd8b057d5fbb06c8e4b0870128
Linh G. Huynh <lhuynh1969@gmail.com>
2020-10-07 09:01:59 -0400

    database work

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-date')
+    # The following shows the 5 most recent blogs
+    #blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs })
